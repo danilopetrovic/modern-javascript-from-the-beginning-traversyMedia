bb643420d608038ed189f848879fa48acb046234
Danilo Petrovic <danilo86petrovic@gmail.com>
2023-01-07 00:00:51 +0100

    added delay for typing request

--- 08 API Projects/073 Github Finder [Part 1] - Intro & UI/githubfinder/app.js
+++ 08 API Projects/073 Github Finder [Part 1] - Intro & UI/githubfinder/app.js
@@ -3,33 +3,37 @@ const github = new Github();
 
 // Search input
 const searchUser = document.getElementById('searchUser');
+let typingTimer;
+const delay = 1000;
 
 // Search input event listener
 searchUser.addEventListener('keyup', (e) => {
-    // Get input text
-    const userText = e.target.value
+    clearTimeout(typingTimer);
 
-    if (userText !== '') {
-        // Make http call
-        github.getUser(userText)
-            .then(data => {
-                if (data.profile.message === 'Not Found') {
-                    // Show alert
-                    console.log('not found....')
+    const doneTyping = () => {
+        // Get input text
+        const userText = e.target.value
 
+        if (userText !== '') {
+            // Make http call
+            github.getUser(userText)
+                .then(data => {
+                    if (data.profile.message === 'Not Found') {
+                        // Show alert
+                        console.log('not found....')
 
+                    } else {
+                        // Show profile
+                        console.log(data);
 
-                } else {
-                    // Show profile
-                    console.log(data);
+                    }
+                });
+        } else {
+            // Clear profile
 
-
-
-                }
-            });
-    } else {
-        // Clear profile
-
+        }
 
     }
+
+    typingTimer = setTimeout(doneTyping, delay);
 })
