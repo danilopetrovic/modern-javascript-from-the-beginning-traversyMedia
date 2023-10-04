19067bee1fcf569abac1718b900f51a834c41281
Danilo Petrovic <danilo86petrovic@gmail.com>
2023-10-04 04:20:20 +0200

    Chuck Norris Joke Generator changed with api-ninjas json app

--- 07 Asynchronous JavaScript, Ajax & Fetch API/061 7-5-project-files - 061 Data From an External API - Chuck Norris Project/chucknorrisjokes/app2-vezba.js
+++ 07 Asynchronous JavaScript, Ajax & Fetch API/061 7-5-project-files - 061 Data From an External API - Chuck Norris Project/chucknorrisjokes/app2-vezba.js
@@ -2,29 +2,32 @@ document.querySelector('.get-jokes').addEventListener('click', function (e) {
     e.preventDefault();
 
     const number = document.querySelector('input[type="number"]').value;
+    var apiKey = 'K1Z/8bkCfxYbv4kEN3PcxA==LTETBGjnFT6CiTkW';
+    var url = 'https://api.api-ninjas.com/v1/jokes?limit=' + number;
     // console.log(number);
 
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+    xhr.open('GET', url, true);
+    xhr.setRequestHeader('X-Api-Key', apiKey);
 
     xhr.onload = function () {
         if (this.status === 200) {
             const response = JSON.parse(this.response);
-            // console.log(response.value);
+            // console.log(response);
 
             let output = '';
-            if (response.type === 'success') {
-                response.value.forEach(function (joke) {
-                    // console.log(joke.joke)
-                    output += `<li>${joke.joke}</li>`
-                });
-                document.querySelector('.jokes').innerHTML = output;
-            } else {
-                output += '<li>something went wrong...</li>'
-            }
-        }
 
+            response.forEach(function (joke) {
+                // console.log(joke.joke)
+                output += `<li>${joke.joke}</li>`
+            });
+            document.querySelector('.jokes').innerHTML = output;
+        }
+    };
+
+    xhr.onerror = function () {
+        console.error('Network error occurred');
     };
 
     xhr.send();
