6c25679320d7e98d5c77d46192319374a2423eb5
Danilo Petrovic <danilo86petrovic@gmail.com>
2023-10-12 21:52:41 +0200

    078 WeatherJS [Part 1] - Intro & UI

--- 08 API Projects/077 Github Finder [Part 5] - Fetch & Display Repos/githubfinder/ui.js
+++ 08 API Projects/077 Github Finder [Part 5] - Fetch & Display Repos/githubfinder/ui.js
@@ -40,7 +40,7 @@ class Ui {
                 <div class="card card-body mb-2">
                     <div class="row">
                         <div class="col-md-6">
-                            <a href="${repo.html_url}" target="_blank">${repo.name}}</a>
+                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                         </div>
                         <div class="col-md-6">
                             <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
