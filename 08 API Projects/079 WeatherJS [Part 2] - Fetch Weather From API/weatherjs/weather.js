ee5cdee26c163ebf252eb3992ed1f64906ed9c12
Danilo Petrovic <danilo86petrovic@gmail.com>
2023-10-16 17:12:43 +0200

    079 WeatherJS [Part 2] - Fetch Weather From API - started with anothr api (www.weatherapi.com)

---
+++ 08 API Projects/079 WeatherJS [Part 2] - Fetch Weather From API/weatherjs/weather.js
@@ -0,0 +1,12 @@
+class Weather {
+    constructor() {
+        // https://www.weatherapi.com/api-explorer.aspx
+        this.apiKey = '609c6c0e2a2e4d4fbac150629231610';
+        this.city = city;
+        this.state = state;
+    }
+
+// Fetch weather from API
+
+
+}
